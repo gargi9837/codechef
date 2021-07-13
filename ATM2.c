e4e90f6cf2d84816a23aec059be637d28ea450de
gargi9837 <75661376+gargi9837@users.noreply.github.com>
2021-07-12 20:26:38 -0700

    Add files via upload

---
+++ ATM2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main(){
+    int t;
+    scanf("%d",t);
+    while(t--){
+    int k,n,i,ar[100];
+    
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++){
+       scanf("%d",&ar[i]);}
+
+    for(i=0;i<n;i++){
+       if(ar[i]<=k)
+          { k=k-ar[i];
+           printf("1");}
+           else
+           printf("0");}
+    }
+       
+
+    }
